24cb653e27e02b4b04c6b657c1f56cd9e8b8b21a
Mah1uzAhmed <mahfuzahmed296@gmail.com>
2026-01-15 23:48:41 +0600

    Base Project

---
+++ openglportable/main.cpp
@@ -0,0 +1,21 @@
+#include <GL/glut.h>
+
+void display()  //this functions name can be anything
+{
+   glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+   glFlush(); // flush whatever has been done
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(640, 500);
+    glutCreateWindow("OpenGL 64bit");
+    glutDisplayFunc(display);  //call the display function
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glutMainLoop();
+    return 0;
+}
